e1ccf73d711b6464b050a43930e4401668c9650f
Yumozh <juliiamozhchil@gmail.com>
2026-04-09 16:17:19 -0500

    feat:print the highest salary

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,5 +1,12 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+        float bobSalary = 25869.94f;
+        float garySalary = 234906.28f;
+        float highestSalary = 0;
 
+        highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("The highest salary is $" + highestSalary);
+    }
 }
