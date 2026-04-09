361e10310e0679b1f19783c1c67b21c38fb997c8
Yumozh <juliiamozhchil@gmail.com>
2026-04-09 16:22:42 -0500

    feat:print the smallest price on transportation

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -2,11 +2,21 @@ package com.pluralsight;
 
 public class MathApp {
     public static void main(String[] args) {
-        float bobSalary = 25869.94f;
-        float garySalary = 234906.28f;
-        float highestSalary = 0;
+        double bobSalary = 258699.94f;
+        double garySalary = 234906.28f;
+        double highestSalary = 0;
 
         highestSalary = Math.max(bobSalary, garySalary);
         System.out.println("The highest salary is $" + highestSalary);
+
+        double carPrice = 35000.94;
+        double truckPrice = 123090.00;
+        double smallestPrice = 0;
+
+        smallestPrice = Math.min(truckPrice, carPrice);
+        System.out.print("The smallest price is $" + smallestPrice);
+
+
+
     }
 }
