c1ddf58867ff263414af0c5c11df21edc6f983ed
Yumozh <juliiamozhchil@gmail.com>
2026-04-09 16:30:28 -0500

    feat: calculate area of a circle

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -4,18 +4,22 @@ public class MathApp {
     public static void main(String[] args) {
         double bobSalary = 258699.94f;
         double garySalary = 234906.28f;
-        double highestSalary = 0;
+        double highestSalary = 0.0;
 
         highestSalary = Math.max(bobSalary, garySalary);
         System.out.println("The highest salary is $" + highestSalary);
 
         double carPrice = 35000.94;
         double truckPrice = 123090.00;
-        double smallestPrice = 0;
+        double smallestPrice = 0.0;
 
         smallestPrice = Math.min(truckPrice, carPrice);
-        System.out.print("The smallest price is $" + smallestPrice);
+        System.out.println("The smallest price is $" + smallestPrice);
 
+        double radius = 7.25;
+        double circleArea = 3.14 * (7.25 * 7.25);
+
+        System.out.printf("Area of a circle equals to %.2f%n",circleArea);
 
 
     }
